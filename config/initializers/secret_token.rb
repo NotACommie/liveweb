6a3f75680c70904e2d45da067d30e74c53f090f9
James Pochas <jpochas@gmail.com>
2013-01-16 03:13:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liveweb::Application.config.secret_token = 'd2497a7a221ef25f7772055e6677c84641a6e3165939bbf8ffa367c0fcd71ac5b37f6673385013bbd5e28dd8991b7f6eb7945db45e6f82abd6470f1b68ce4b70'
